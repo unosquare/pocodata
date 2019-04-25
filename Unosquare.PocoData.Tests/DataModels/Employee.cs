9a663dd6535044d99351bcbbbe4eca6634edc8c7
JCorrea13 <josemanuelcorrea.13@gmail.com>
2019-04-25 13:33:05 -0500

    setup

---
+++ Unosquare.PocoData.Tests/DataModels/Employee.cs
@@ -0,0 +1,49 @@
+﻿namespace Unosquare.PocoData.Sample.DataModels
+{
+    using System;
+    using Unosquare.PocoData.Annotations;
+
+    [Table("Employees")]
+    public class Employee
+    {
+        public Employee()
+        {
+            // placeholder
+        }
+
+        public Employee(int id, string fullName, string emailAddress, DateTime dob, int? children, object reserved)
+            : this()
+        {
+            EmployeeId = id;
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            DateOfBirth = dob;
+            Children = children;
+            Reserved = reserved;
+            HashCode = this.GetHashCode();
+        }
+
+        [Key(true)]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string FullName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int? Children { get; set; }
+
+        public object Reserved { get; set; }
+
+        [NotMapped]
+        public int HashCode { get; set; }
+
+        public override string ToString()
+        {
+            return $"{EmployeeId} {FullName} {EmailAddress} {DateOfBirth} {Children}";
+        }
+    }
+}
---
+++ Unosquare.PocoData.Tests/SimpleTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlServerCe;
+using System.IO;
+using System.Text;
+using Unosquare.PocoData.Sql;
+using Xunit;
+
+namespace Unosquare.PocoData.Tests
+{
+    public class SimpleTest
+    {
+        private string connectionString { get; set; }
+        public SimpleTest()
+        {
+
+
+            var fileName = Path.GetTempPath() + "pocoData.sdf";
+            var password = "pocodata";
+
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            connectionString = String.Format("DataSource = \"{0}\"", fileName, password);
+            SqlCeEngine en = new SqlCeEngine(connectionString);
+            en.CreateDatabase();
+
+        }
+
+        [Fact]
+        public void Connection_test()
+        {
+
+            // var MyDB = new SqlPocoDb(connectionString);
+
+            Assert.True(true, "");
+        }
+
+    }
+}
