1222fbb34ecbac5a786348951afeeb1bd583d1fe
JCorrea13 <josemanuelcorrea.13@gmail.com>
2019-08-28 16:23:08 -0500

    adding DbTest this class claer and initialize the database for each test

---
+++ Unosquare.PocoData.Tests/DbTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unosquare.PocoData.Tests.DataModels;
+
+namespace Unosquare.PocoData.Tests
+{
+    class DbTest : IDisposable
+    {
+        public DbTest(): this(true) { }
+
+        public DbTest(bool initialice)
+        {
+            if (!initialice)
+            {
+                return;
+            }
+
+            using (var db = new SampleDb())
+            {
+                var data = new Employee()
+                {
+                    FullName = "José Correa",
+                    EmailAddress = "jose.correa@unosquare.com",
+                    Children = 0,
+                    DateOfBirth = new DateTime(1995, 6, 12)
+                };
+
+                db.Employees.Insert(data, false);
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var db = new SampleDb())
+            {
+                var employees = db.Employees.SelectAll();
+                foreach (var employee in employees)
+                    db.Employees.Delete(employee);
+            }
+        }
+    }
+}
