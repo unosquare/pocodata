06850f2e547fec30d0a5b71955b02c75c31a69ff
JCorrea13 <josemanuelcorrea.13@gmail.com>
2019-08-28 16:15:10 -0500

    adding basic tests

--- Unosquare.PocoData.Tests/SimpleTest.cs
+++ Unosquare.PocoData.Tests/SimpleTest.cs
@@ -1,5 +1,8 @@
 ﻿namespace Unosquare.PocoData.Tests
 {
+    using System;
+    using System.Linq;
+    using Unosquare.PocoData.Tests.DataModels;
     using Xunit;
 
     public class SimpleTest
@@ -10,6 +13,48 @@
             using (var db = new SampleDb())
             {
                 Assert.True(db.Connection?.State == System.Data.ConnectionState.Open);
+            }
+        }
+
+        [Fact]
+        public void ValidInsert()
+        {
+            using (var db = new SampleDb())
+            {
+                var data = new Employee()
+                {
+                    FullName = "José Correa",
+                    EmailAddress = "jose.correa@unosquare.com",
+                    Children = 0,
+                    DateOfBirth = new DateTime(1995, 6, 12)
+                };
+
+                var result = db.Employees.Insert(data, false);
+                Assert.True(data.EmployeeId != 0);
+            }
+        }
+
+        [Fact]
+        public void ValidSelect()
+        {
+            using (var db = new SampleDb())
+            {
+                var data = db.Employees.SelectAll();
+                Assert.NotEmpty(data);
+            }
+        }
+
+        [Fact]
+        public void ValidUpdate()
+        {
+            using (var db = new SampleDb())
+            {
+                var toUpdate = db.Employees.SelectAll().FirstOrDefault();
+                var newValue = new Random().Next(100);
+                toUpdate.Children = newValue;
+
+                db.Update(toUpdate);
+                Assert.True(toUpdate.Children == newValue);
             }
         }
 
